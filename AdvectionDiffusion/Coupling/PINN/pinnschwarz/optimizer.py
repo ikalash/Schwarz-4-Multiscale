afde540756f95fca4d201ce9031ed68c1deb550c
Jinny Chung <jinnyc1127@gmail/com>
2023-10-22 14:10:48 -0700

    testing Pe=100

--- AdvectionDiffusion/Coupling/PINN/pinnschwarz/optimizer.py
+++ AdvectionDiffusion/Coupling/PINN/pinnschwarz/optimizer.py
@@ -3,18 +3,19 @@ import driver
 
 search_space = {
     # enter param space
-    "hl": tune.grid_search([2, 3]),
-    "nl": tune.grid_search([10, 20])
+    "hl": tune.grid_search(range(2, 11)),
+    "nl": tune.grid_search(range(10, 85, 10))
 }
 
 def objective(config):
-    cpu_time = driver.Driver("/Users/jinnychung/Documents/Stanford/Sandia/Schwarz-4-Multiscale/AdvectionDiffusion/Coupling/PINN/cases/example/input.csv",
-                             "/Users/jinnychung/Documents/Stanford/Sandia/test",
-                             "/Users/jinnychung/Documents/Stanford/Sandia/Schwarz-4-Multiscale/AdvectionDiffusion/Coupling/PINN/pinnschwarz/hyper.yaml",
-                             make_fig=False,
-                             hl=config['hl'],
-                             nl=config['nl']).train()
-    return {"cpu": cpu_time} # TODO: need to look at loss
+    cpu_time = driver.Driver(
+        "/home/users/jinnyc/Schwarz-4-Multiscale/AdvectionDiffusion/Coupling/PINN/cases/example/input.csv",
+        "/scratch/users/jinnyc/test/optimize",
+        "/home/users/jinnyc/Schwarz-4-Multiscale/AdvectionDiffusion/Coupling/PINN/pinnschwarz/hyper.yaml",
+        make_fig=False,
+        hl=config['hl'],
+        nl=config['nl']).train()
+    return {"cpu": cpu_time}
 
 tuner = tune.Tuner(objective, param_space=search_space)
 results = tuner.fit()
